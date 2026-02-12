9a59ddb7b77032721e41c59cec26c261caf28445
jinghaihan <jhh19980114@gmail.com>
2026-02-12 12:27:15 +0800

    chore: split constants

--- src/constants/package-manager.ts
+++ src/constants/package-manager.ts
@@ -86,13 +86,3 @@ export const PACKAGE_MANAGER_CONFIG: Record<PackageManager, PackageManagerConfig
     defaultContent: '{}',
   },
 }
-
-export const CMD_BOOL_FLAGS = new Set([
-  'save-dev',
-  'save-peer',
-  'save-optional',
-  'save-exact',
-  'recursive',
-])
-
-export const CMD_BOOL_SHORT_FLAGS = new Set(['D', 'P', 'O', 'E', 'r'])
---
+++ src/constants/process.ts
@@ -0,0 +1,9 @@
+export const CMD_BOOL_FLAGS = new Set([
+  'save-dev',
+  'save-peer',
+  'save-optional',
+  'save-exact',
+  'recursive',
+])
+
+export const CMD_BOOL_SHORT_FLAGS = new Set(['D', 'P', 'O', 'E', 'r'])
