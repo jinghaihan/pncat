fe854f88ed75f7031756e034282aecef1fabc869
jinghaihan <jhh19980114@gmail.com>
2026-03-31 11:16:07 +0800

    chore: remove semver alias plugin

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'tsdown'
-import { semverAliasPlugin } from './build/semver'
 
 export default defineConfig({
   entry: [
@@ -12,9 +11,6 @@ export default defineConfig({
   deps: {
     onlyBundle: false,
   },
-  plugins: [
-    semverAliasPlugin(),
-  ],
   exports: true,
   clean: true,
 })
